5f66e4c10d0b43f6fc6dc42fe6b996c4573ea15f
Nemat Aka <yorqinkelajak74@gmail.com>
2026-06-09 16:39:55 +0500

    ADMIN_IDS тузатилди

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
@@ -9,5 +10,5 @@ DB_NAME = os.getenv('DB_NAME', 'scooter.db')
 # Овнер (эгаси)
 OWNER_ID = 6057285077
 
-# Бошқа админлар (овнердан ташқари)
-ADMIN_IDS = []  # Бошқа админлар ID'си, масалан: [123456789, 987654321]
+# Админлар рўйхати (овнер ҳам админ)
+ADMIN_IDS = [6057285077]  # Овнер ID си қўшилган!
